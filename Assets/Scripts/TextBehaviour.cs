a2e1da3c3d394ff1d52a7787b1134a656fa4a94a
Dmytro Piskarov <dima.piskariov@gmail.com>
2023-02-27 20:59:46 +0200

    Added number counter (for test)

--- Assets/Scripts/TextBehaviour.cs
+++ Assets/Scripts/TextBehaviour.cs
@@ -1,20 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class TextBehaviour : MonoBehaviour
 {
+    public TextMeshProUGUI myText;
+    private int textNumber = 0;
     
-    
-    // Start is called before the first frame update
     void Start()
     {
-        
+        myText.text = textNumber < 10 ? $"0{textNumber}" : textNumber.ToString();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            textNumber++;
+            myText.text = textNumber < 10 ? $"0{textNumber}" : textNumber.ToString();
+        }
     }
 }
